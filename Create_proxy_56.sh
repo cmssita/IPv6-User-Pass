351781d9fa80f6299257e0bb0b86f4d9963e4887
cmssita <121331068+cmssita@users.noreply.github.com>
2023-01-21 18:50:00 +0700

    Update Create_proxy_56.sh

--- Create_proxy_56.sh
+++ Create_proxy_56.sh
@@ -170,9 +170,9 @@ wget "https://raw.githubusercontent.com/cmssita/My-Prox/main/Rotation_56.sh" --o
 chmod 777 /root/Rotation.sh
 cat >>/var/spool/cron/root<<EOF
 #day - time
-59 7 * * * /root/Rotation.sh > /root/Rotation_log.txt
+#59 7 * * * /root/Rotation.sh > /root/Rotation_log.txt
 #minutes
 #*30 * * * * /root/Rotation.sh > /root/Rotation_log.txt
 #hour
-#0 * * * * /root/Rotation.sh > /root/Rotation_log.txt
+0 */15 * * * /root/Rotation.sh > /root/Rotation_log.txt
 EOF
