9d6d4a09780debf9b778ca43e6ee2dcd60b865a1
cmssita <121331068+cmssita@users.noreply.github.com>
2023-02-27 17:23:16 +0700

    Update Create_proxy.sh

--- Create_proxy.sh
+++ Create_proxy.sh
@@ -127,12 +127,12 @@ interface=eth0
 #done
 Auth=strong
 User=MKproxy
-Pass=Mkpasswd
+Pass=MKpasswd
 
 #read -p "Please input start port :" FIRST_PORT
 #read -p "Please input start port :" LAST_PORT
 FIRST_PORT=30000
-LAST_PORT=35000
+LAST_PORT=33000
 
 rm -fv $WORKDIR/ipv6-subnet.txt
 cat >>$WORKDIR/ipv6-subnet.txt <<EOF
