f90242b34a14e70b8660361c8558932ede66d8fd
cmssita <121331068+cmssita@users.noreply.github.com>
2023-03-05 09:49:26 +0700

    Update Create_proxy_56.sh

--- Create_proxy_56.sh
+++ Create_proxy_56.sh
@@ -6,9 +6,6 @@ random() {
 
 array=(1 2 3 4 5 6 7 8 9 0 a b c d e f)
 gen64() {
-	ip64() {
-		echo "${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}"
-	}
 	ip56() {
 		echo "${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}"
 	}
