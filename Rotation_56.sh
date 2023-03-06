0b41023579b7ec555795a2c37ffe5eff5ae15bb5
cmssita <121331068+cmssita@users.noreply.github.com>
2023-03-06 15:27:36 +0700

    Update Rotation_56.sh

--- Rotation_56.sh
+++ Rotation_56.sh
@@ -98,7 +98,7 @@ Auth=$(awk -F "|" '{print $6}' ${WORKDATA2})
 #FIRST_PORT=$(awk -F "|" '{print $7}' ${WORKDATA2})
 #LAST_PORT=$(awk -F "|" '{print $7}' ${WORKDATA2})
 FIRST_PORT=30000
-LAST_PORT=31000
+LAST_PORT=33000
 
 systemctl restart network
 systemctl start NetworkManager.service
